796ee77c3d51811cae4234ab93fed394916f0cf3
B. Abril Castañeda Fernández <146761790+BAteneacf@users.noreply.github.com>
2026-06-06 18:18:34 -0600

    Cuestionario
    Se entregan los cuestionarios y ejercicios de la Academia Full Stack Q2

---
+++ src/Semana_1/Ejercicio_1.java
@@ -0,0 +1,24 @@
+package Semana_1;
+
+public class Ejercicio_1 {
+    public static void main(String[] args) {
+        //TODO: Declarar variables
+        String nombre = "Abril Castañeda";
+        int edad = 31;
+        double altura = 1.53;
+        boolean esActivo = true;
+
+        //TODO: Concatenacion con +
+        String mensaje1 = "Me llamo " + nombre + ", tengo " + edad
+                + " anios, mido " + altura + "m y estoy "
+                + (esActivo ? "activo" : "inactivo") + ".";
+        System.out.println(mensaje1);
+
+        //TODO: Usando String.format()
+        String mensaje2 = String.format(
+                "Me llamo %s, tengo %d anios, mido %.2f m y estoy %s.",
+                nombre, edad, altura, esActivo ? "activo" : "inactivo"
+        );
+        System.out.println(mensaje2);
+    }
+}
